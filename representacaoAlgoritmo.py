05d00f19bc4f21c9684e369df07045ae4eac8118
Salocinrevenge <nicolashecker.nhs@gmail.com>
2022-07-18 17:26:49 -0300

    inicio do pygame

---
+++ representacaoAlgoritmo.py
@@ -0,0 +1,19 @@
+import pygame       #usando pygame, usar com pip ou baixar separado
+from pygame.locals import *     #necessario para eventos definidos, como o QUIT
+
+ALTURA = 600    #altura
+LARGURA = 1000  #largura
+pygame.init()   #iniciar pygame
+screen = pygame.display.set_mode((LARGURA, ALTURA)) #criar a janela
+
+pygame.display.set_caption('resolver labirinto')    #dar nome a janela
+
+clock = pygame.time.Clock() #setar maquinad e fps
+
+while True: #loop do game
+    clock.tick(20)  #fps
+    for event in pygame.event.get():    #eventos como clicar em botoes ou mouse
+        if event.type == QUIT:  
+            pygame.quit()   #fechar jogo ao sair
+    screen.fill((0, 0, 0))  #pintar tela de preto
+    pygame.display.update() #atualizar pintura
